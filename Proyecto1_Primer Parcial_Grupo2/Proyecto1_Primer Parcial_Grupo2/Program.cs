e23a62f10f48c0132cd4c60fea497cba3ac2d60d
Nyxby2002 <nyxby2002@gmail.com>
2025-06-04 12:52:31 -0600

    Ejercicio-12-Terminado

--- Proyecto1_Primer Parcial_Grupo2/Proyecto1_Primer Parcial_Grupo2/Program.cs
+++ Proyecto1_Primer Parcial_Grupo2/Proyecto1_Primer Parcial_Grupo2/Program.cs
@@ -4,14 +4,29 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Este es la Rama principal, el cual sera la base de datos de todos los demas ejercicios.");
-        Console.WriteLine("Este proyecto es del grupo 2 del primer parcial de Programación 1.");
-        Console.WriteLine("Integrantes del grupo:");
-        Console.WriteLine("1. Bryan Galo");
-        Console.WriteLine("2. Econ Betancourth");
-        Console.WriteLine("3. Josue Sauceda");
-        Console.WriteLine("4. Javier Ponce");
-        //no tocar esta linea, ya que es la rama principal del proyecto por lo tanto debemos de crear una rama para cada ejercicio.
-        Console.WriteLine("Para poder ver los diferentes ejercicios del proyecto por favor dirígete a las ramas del repositorio.");
+        Random random = new Random();
+        int numeroAdivinar = random.Next(1, 1001);
+        int intento = 0;
+
+        Console.WriteLine("Adivina el número entre 1 y 1000:");
+
+        while (true)
+        {
+            intento = Convert.ToInt32(Console.ReadLine());
+
+            if (intento < numeroAdivinar)
+            {
+                Console.WriteLine("Demasiado bajo. Intenta de nuevo.");
+            }
+            else if (intento > numeroAdivinar)
+            {
+                Console.WriteLine("Demasiado alto. Intenta de nuevo.");
+            }
+            else
+            {
+                Console.WriteLine("¡Excelente! ¡Adivinaste el número!");
+                break;
+            }
+        }
     }
 }
