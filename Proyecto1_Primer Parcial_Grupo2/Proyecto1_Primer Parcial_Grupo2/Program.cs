5585aff20512eaab9ef0f01171e934dbb9cbd70a
Nyxby2002 <nyxby2002@gmail.com>
2025-06-04 11:20:12 -0600

    Rama Principal

---
+++ Proyecto1_Primer Parcial_Grupo2/Proyecto1_Primer Parcial_Grupo2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Este es la Rama principal, el cual sera la base de datos de todos los demas ejercicios.");
+        Console.WriteLine("Este proyecto es del grupo 2 del primer parcial de Programación 1.");
+        Console.WriteLine("Integrantes del grupo:");
+        Console.WriteLine("1. Bryan Galo");
+        Console.WriteLine("2. Econ Betancourth");
+        Console.WriteLine("3. Josue Sauceda");
+        Console.WriteLine("4. Javier Ponce");
+        //no tocar esta linea, ya que es la rama principal del proyecto por lo tanto debemos de crear una rama para cada ejercicio.
+        Console.WriteLine("Para poder ver los diferentes ejercicios del proyecto por favor dirígete a las ramas del repositorio.");
+    }
+}
