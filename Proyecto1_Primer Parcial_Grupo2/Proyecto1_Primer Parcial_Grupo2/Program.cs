caf19bef999b7021084f44a2d3641fad60e151fe
Nyxby2002 <nyxby2002@gmail.com>
2025-06-04 12:42:42 -0600

    Ejercicio-10-Terminado

--- Proyecto1_Primer Parcial_Grupo2/Proyecto1_Primer Parcial_Grupo2/Program.cs
+++ Proyecto1_Primer Parcial_Grupo2/Proyecto1_Primer Parcial_Grupo2/Program.cs
@@ -4,14 +4,28 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Este es la Rama principal, el cual sera la base de datos de todos los demas ejercicios.");
-        Console.WriteLine("Este proyecto es del grupo 2 del primer parcial de Programación 1.");
-        Console.WriteLine("Integrantes del grupo:");
-        Console.WriteLine("1. Bryan Galo");
-        Console.WriteLine("2. Econ Betancourth");
-        Console.WriteLine("3. Josue Sauceda");
-        Console.WriteLine("4. Javier Ponce");
-        //no tocar esta linea, ya que es la rama principal del proyecto por lo tanto debemos de crear una rama para cada ejercicio.
-        Console.WriteLine("Para poder ver los diferentes ejercicios del proyecto por favor dirígete a las ramas del repositorio.");
+        Console.WriteLine("Grados Centígrados a Fahrenheit:");
+        Console.WriteLine("Celsius\tFahrenheit");
+        for (int celsius = 0; celsius <= 100; celsius++)
+        {
+            Console.WriteLine($"{celsius}\t{CelsiusToFahrenheit(celsius)}");
+        }
+
+        Console.WriteLine("\nGrados Fahrenheit a Centígrados:");
+        Console.WriteLine("Fahrenheit\tCelsius");
+        for (int fahrenheit = 32; fahrenheit <= 212; fahrenheit++)
+        {
+            Console.WriteLine($"{fahrenheit}\t{FahrenheitToCelsius(fahrenheit)}");
+        }
+    }
+
+    static double CelsiusToFahrenheit(int celsius)
+    {
+        return (celsius * 9.0 / 5.0) + 32;
+    }
+
+    static double FahrenheitToCelsius(int fahrenheit)
+    {
+        return (fahrenheit - 32) * 5.0 / 9.0;
     }
 }
