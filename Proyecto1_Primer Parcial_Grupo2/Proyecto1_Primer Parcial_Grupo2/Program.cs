05fde282b68d2cf214cad26e89da1f3cc6baef2a
Nyxby2002 <nyxby2002@gmail.com>
2025-06-04 12:48:13 -0600

    Ejercicio-11-Terminado

--- Proyecto1_Primer Parcial_Grupo2/Proyecto1_Primer Parcial_Grupo2/Program.cs
+++ Proyecto1_Primer Parcial_Grupo2/Proyecto1_Primer Parcial_Grupo2/Program.cs
@@ -4,14 +4,30 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Este es la Rama principal, el cual sera la base de datos de todos los demas ejercicios.");
-        Console.WriteLine("Este proyecto es del grupo 2 del primer parcial de Programación 1.");
-        Console.WriteLine("Integrantes del grupo:");
-        Console.WriteLine("1. Bryan Galo");
-        Console.WriteLine("2. Econ Betancourth");
-        Console.WriteLine("3. Josue Sauceda");
-        Console.WriteLine("4. Javier Ponce");
-        //no tocar esta linea, ya que es la rama principal del proyecto por lo tanto debemos de crear una rama para cada ejercicio.
-        Console.WriteLine("Para poder ver los diferentes ejercicios del proyecto por favor dirígete a las ramas del repositorio.");
+        Console.WriteLine("Ingrese 3 notas:");
+        double nota1 = Convert.ToDouble(Console.ReadLine());
+        double nota2 = Convert.ToDouble(Console.ReadLine());
+        double nota3 = Convert.ToDouble(Console.ReadLine());
+
+        double promedio = (nota1 + nota2 + nota3) / 3;
+
+        switch (promedio)
+        {
+            case double n when (n < 70):
+                Console.WriteLine("Reprueba");
+                break;
+            case double n when (n <= 80):
+                Console.WriteLine("Bueno");
+                break;
+            case double n when (n <= 90):
+                Console.WriteLine("Muy Bueno");
+                break;
+            case double n when (n <= 100):
+                Console.WriteLine("Sobresaliente");
+                break;
+            default:
+                Console.WriteLine("Error");
+                break;
+        }
     }
 }
